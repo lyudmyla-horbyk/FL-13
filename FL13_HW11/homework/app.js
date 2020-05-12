667bb969ed1c423b1fbe7dff269f1974df909e15
Liudmyla Liashuk <lyudmyla.horbyk@gmail.com>
2020-05-12 22:43:25 +0300

    my eleventh homework

--- FL13_HW11/homework/app.js
+++ FL13_HW11/homework/app.js
@@ -59,15 +59,78 @@ function createTree(root, arr) {
 function createTreeDom(arr) {
   let ul = document.createElement('ul');
   for (let i = 0; i < arr.length; i++) {
+    let container = document.createElement('span');
+    let icon = document.createElement('i');
+    if (arr[i].folder !== true) {
+      icon.innerHTML = 'insert_drive_file';
+      icon.className = 'material-icons file-icon';
+    } else {
+      icon.innerHTML = 'folder';
+      icon.className = 'material-icons folder-icon';
+    }
+    container.append(icon, arr[i].title);
     let li = document.createElement('li');
-    li.innerHTML = arr[i].title;
+    if (arr[i].folder !== true) {
+      li.className = 'file';
+    } else {
+      li.className = 'folder';
+      container.addEventListener('click', function () {
+        arr[i].opened = !arr[i].opened;
+        if (arr[i].opened === true) {
+          icon.innerHTML = 'folder_open';
+          if (li.lastChild.nodeName === 'UL' || li.lastChild.nodeName === 'DIV') {
+            li.lastChild.classList.remove('hidden');
+          }
+        } else {
+          icon.innerHTML = 'folder';
+          if (li.lastChild.nodeName === 'UL' || li.lastChild.nodeName === 'DIV') {
+            li.lastChild.classList.add('hidden');
+          }
+        }
+      })
+    }
+    let menuBlock = document.createElement('div');
+    menuBlock.classList.add('menu-block')
+    const renameButton = document.createElement('button');
+    renameButton.append('Rename')
+    const deleteButton = document.createElement('button');
+    deleteButton.append('Delete item')
+    menuBlock.append(renameButton)
+    menuBlock.append(deleteButton)
+    container.append(menuBlock);
+    container.addEventListener('contextmenu', event => {
+      event.preventDefault();
+      event.stopPropagation();
+      menuBlock.classList.toggle('active')
+    }, false);
+
+    menuBlock.addEventListener('click', function (e) {
+      e.stopPropagation();
+    })
+
+    document.addEventListener('click', event => {
+      if (!event.target !== menuBlock) {
+        menuBlock.classList.remove('active');
+      }
+    }, false);
+
+    li.append(container);
     if (Array.isArray(arr[i].children)) {
       let childrenUl = createTreeDom(arr[i].children);
+      childrenUl.classList.add('hidden')
       if (childrenUl) {
         li.append(childrenUl);
       }
+    } else if (arr[i].folder === true && arr[i].children === null) {
+      let folderIsEmpty = document.createElement('div');
+      folderIsEmpty.classList.add('hidden')
+      folderIsEmpty.innerHTML = 'Folder is empty';
+      folderIsEmpty.classList.add('text');
+      li.append(folderIsEmpty);
     }
+
     ul.append(li);
+
   }
 
   return ul;
